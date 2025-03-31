2cef1e8c2722c4ce12a99b872e8460a1bebaf8c1
Ignacio Andana De Lapyera <Ignacioad92@gmail.com>
2025-03-31 18:35:53 -0300

    [UPDATE] elimina excepcion sin usar

--- src/main/java/com/example/veterinary_billing/exception/ParticipantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.veterinary_billing.exception;
-
-public class ParticipantNotFoundException extends RuntimeException {
-    public ParticipantNotFoundException(String message) {
-        super(message);
-    }
-}

e8d8f07521ba3faf206c9c89e8f34aafb85ebdc2
Nashoooh-stech <ignacio@stech.guru>
2025-04-16 08:59:32 -0400

    [UPDATE] actualiza el VeterinaryBillingApplication por FacturaVeterinariaApplication

--- src/main/java/com/example/veterinary_billing/FacturaVeterinariaApplication.java
+++ src/main/java/com/example/veterinary_billing/FacturaVeterinariaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VeterinaryBillingApplication {
+public class FacturaVeterinariaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VeterinaryBillingApplication.class, args);
+		SpringApplication.run(FacturaVeterinariaApplication.class, args);
 	}
 
 }
